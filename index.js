5ab99108a01d2021de3951af57e0fe701de793c1
Nick Gromyko <nxgr.dev@gmail.com>
2024-06-24 23:14:34 +0500

    add colors update gc (get-color) command

--- index.js
+++ index.js
@@ -5,13 +5,25 @@ const { hideBin } = require("yargs/helpers")
 const colors = require('./src/const/colors');
 
 yargs(hideBin(process.argv))
-    .command('gcn [color]', 'get tailwind color name', (yargs) => {
+    .command('gc [color]', 'get tailwind color name', (yargs) => {
         yargs.positional('color', {
             alias: 'c',
             description: 'any hex value'
         })
     }, (argv) => {
-        console.log(colors[argv.color]);
-    })
+        let color = String(argv.color)
+        if (!color.startsWith('#')) {
+            color = `#${color}`
+        }
+        if (!/^#([0-9a-fA-F]{3}|[0-9a-fA-F]{6})$/.test(color)) {
+            console.log(`Invalid color "${color}"`)
+            return
+        }
 
+        if (colors[color]) {
+            console.log(colors[color])
+        } else {
+            console.log(`There is no color "${color}"`)
+        }
+    })
     .parse()
